5c74e43e337c3605839d2cea39daebd6b95628ef
yawn-yawn-yawn <yawn.yawn.yawn.84@gmail.com>
2020-04-28 19:28:20 +0900

    新しいテーマを作り始めました

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # tumblr-theme
 
+## theme 1
+最初に作ったやつ。
+
+## theme 2
+次に作るやつ
+
 ## markdownの適用
 .post-bodyってクラスのしたならば、適用できます
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
-// モジュールの読み込み
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-// タスクを作成
-// gulp.task("タスク名", 実行される処理)
+var theme = "theme2"
+
 gulp.task("sass", function () {
     return (
-        gulp.src("src/sass/**/*.scss") // 取得するファイル
+        gulp.src(`${theme}/src/sass/**/*.scss`) // 取得するファイル
             .pipe(sass({ outputStyle: "expanded" })) // コンパイル時のオプション
-            .pipe(gulp.dest("src/css")) // 保存先
+            .pipe(gulp.dest(`${theme}/src/css`)) // 保存先
     );
 });
 
 gulp.task("sass-watch", function () {
-    return gulp.watch("src/sass/**/*.scss", function () {
+    return gulp.watch(`${theme}/src/sass/**/*.scss`, function () {
         return (
-            gulp.src("src/sass/**/*.scss")
+            gulp.src(`${theme}/src/sass/**/*.scss`)
                 .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-                .pipe(gulp.dest("src/css"))
+                .pipe(gulp.dest(`${theme}/src/css`))
         );
     });
 });
